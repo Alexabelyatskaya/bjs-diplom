fe54e8ec4e63a9dda254efdbb0d051efb72da6af
Alexandra <alexabelyatskaya@yandex.ru>
2021-01-05 00:02:02 +0300

    Готовый диплом

--- public/homePage.js
+++ public/homePage.js
@@ -1,7 +1,6 @@
 'use strict'
 
 const exitObject = new LogoutButton();
-
 exitObject.action = function() {
 	let callback = (response) => {
 		if (response.success) {
@@ -30,14 +29,12 @@ let refresh = () => {
 	}
 	ApiConnector.getStocks(rateCallback);
 };
-
 refresh();
 setInterval(refresh, 60000);
 
+
 const moneyObject = new MoneyManager();
-
 let addingFunds = (data) => {
-
 	let callback = (response) => {
 	if (response.success) {
 		ProfileWidget.showProfile(response.data);
@@ -48,5 +45,71 @@ let addingFunds = (data) => {
 };
 	ApiConnector.addMoney(data, callback);
 };
-
 moneyObject.addMoneyCallback = addingFunds;
+
+
+let conversion = (data) => {
+	let callback = (response) => {
+	if (response.success) {
+		ProfileWidget.showProfile(response.data);
+		moneyObject.setMessage(true, 'Счет пополнен!');
+	} else {
+		moneyObject.setMessage(false, response.error);
+	}
+};
+	ApiConnector.convertMoney(data, callback);
+}
+moneyObject.conversionMoneyCallback = conversion;
+
+
+let transfer = (data) => {
+	let callback = (response) => {
+	if (response.success) {
+		ProfileWidget.showProfile(response.data);
+		moneyObject.setMessage(true, 'Счет пополнен!');
+	} else {
+		moneyObject.setMessage(false, response.error);
+	}
+};
+	ApiConnector.transferMoney(data, callback);
+}
+moneyObject.sendMoneyCallback = transfer;
+
+
+const favoritesWidget = new FavoritesWidget();
+	let favCallback = (response) => {
+		favoritesWidget.clearTable();
+		favoritesWidget.fillTable(response.data);
+		moneyObject.updateUsersList(response.data);
+	};
+ApiConnector.getFavorites(favCallback);
+
+
+let addUser = (data) => {
+	let callback = (response) => {
+		if (response.success) {
+			favoritesWidget.clearTable();
+			favoritesWidget.fillTable(response.data);
+			moneyObject.updateUsersList(response.data);
+		} else {
+			favoritesWidget.setMessage(false, response.error);
+		}
+	}	
+	ApiConnector.addUserToFavorites(data, callback);
+}
+favoritesWidget.addUserCallback = addUser;
+
+
+let removeUser = (id) => {
+	let callback = (response) => {
+		if (response.success) {
+			favoritesWidget.clearTable();
+			favoritesWidget.fillTable(response.data);
+			moneyObject.updateUsersList(response.data);
+		} else {
+			favoritesWidget.setMessage(false, response.error);
+		}
+	}
+	ApiConnector.removeUserFromFavorites(id, callback);
+}
+favoritesWidget.removeUserCallback = removeUser;
